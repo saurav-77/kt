f6dddc9d337d0f404a1f8dcdb45639596be6e6d5
saurav-77 <srvkrrr@gmail.com>
2025-03-28 18:37:22 +0530

    .

--- Extensions.kt
+++ Extensions.kt
@@ -1,22 +1,5 @@
 fun String.toStringBuilder(): StringBuilder = StringBuilder(this)
 
-fun Array<Int>.toLongArray(): Array<Long> {
-    var res = Array<Long>(this.size) { 0 };
-    for (i in 0 until this.size) {
-        res[i] = this[i].toLong()
-    }
-    return res;
-}
-
-fun IntArray.toLongArray(): LongArray {
-    var n = this.size;
-    var res = LongArray(n) { 0 };
-    for (i in 0..n - 1) {
-        res[i] = this[i].toLong()
-    }
-    return res;
-}
-
 fun Int.toStringBuilder(): StringBuilder = StringBuilder(this.toString())
 
 fun Long.toStringBuilder(): StringBuilder = StringBuilder(this.toString())
